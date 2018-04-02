373465ea4b64eac21eeb7beaf430403122f8bc79
nomad1072 <siddharthlanka@gmail.com>
2018-04-03 01:12:24 +0530

    Minors

--- test.py
+++ test.py
@@ -117,7 +117,7 @@ def single_img_features(img,color_space='RGB',orient=9,pix_per_cell=8,cell_per_b
         
         img_features.append(hog_features)
     
-    return img_features
+    return np.concatenate(img_features)
 
 ycrcb_vehicle_img = cv2.cvtColor(vehicle_img, cv2.COLOR_RGB2YCrCb)
 ycrcb_non_vehicle_img = cv2.cvtColor(non_vehicle_img, cv2.COLOR_RGB2YCrCb)
@@ -183,77 +183,52 @@ t2 = time.time()
 print(round(t2-t, 5), 'Seconds to predict', n_predict, 'labels with SVC')
 
 def draw_boxes(img, bboxes, color=(0, 255, 0), thick=3):
-    # Make a copy of the image
     draw_img = np.copy(img)
-    
-    # Iterate through the bounding boxes
     for bbox in bboxes:
-        # Draw a rectangle given bbox coordinates
         cv2.rectangle(draw_img, bbox[0], bbox[1], color, thick)
-    
-    # Return the image copy with boxes drawn
     return draw_img
 
-# Window size (x and y dimensions), and overlap fraction (for both x and y)
-def slide_window(img, x_start_stop=[None, None], y_start_stop=[None, None], xy_window=(64, 64), xy_overlap=(0.5, 0.5)):
-    # Compute the span of the region to be searched    
+def slide_window(img, x_start_stop=[None, None], y_start_stop=[None, None], xy_window=(64,64), xy_overlap=(0.5,0.5)):
     xspan = x_start_stop[1] - x_start_stop[0]
     yspan = y_start_stop[1] - y_start_stop[0]
-    
-    # Compute the number of pixels per step in x/y
+
     nx_pix_per_step = np.int(xy_window[0]*(1 - xy_overlap[0]))
     ny_pix_per_step = np.int(xy_window[1]*(1 - xy_overlap[1]))
-    
-    # Compute the number of windows in x/y
+
     nx_buffer = np.int(xy_window[0]*(xy_overlap[0]))
     ny_buffer = np.int(xy_window[1]*(xy_overlap[1]))
     nx_windows = np.int((xspan-nx_buffer)/nx_pix_per_step) 
-    ny_windows = np.int((yspan-ny_buffer)/ny_pix_per_step) 
-    
-    # Initialize a list to append window positions to
+    ny_windows = np.int((yspan-ny_buffer)/ny_pix_per_step)
+
     window_list = []
-    
-    # Loop through finding x and y window positions
-    # Note: you could vectorize this step, but in practice
-    # you'll be considering windows one by one with your
-    # classifier, so looping makes sense
+
     for ys in range(ny_windows):
         for xs in range(nx_windows):
-            # Calculate window position
             startx = xs*nx_pix_per_step + x_start_stop[0]
             endx = startx + xy_window[0]
             starty = ys*ny_pix_per_step + y_start_stop[0]
             endy = starty + xy_window[1]
             
-            # Append window position to list
             window_list.append(((startx, starty), (endx, endy)))
             
     # Return the list of windows
     return window_list
 
 def search_windows(img, windows):
-
-    #1) Create an empty list to receive positive detection windows
     on_windows = []
-    
-    #2) Iterate over all windows in the list
+
     for window in windows:
-        #3) Extract the test window from original image
-        test_img = cv2.resize(img[window[0][1]:window[1][1], window[0][0]:window[1][0]], (64, 64))      
-        
-        #4) Extract features for that window using single_img_features()
-        features = single_img_features(test_img, color_space, orient, pix_per_cell, cell_per_block, hog_channel)
-        #5) Scale extracted features to be fed to classifier
-        scaler = StandardScaler().fit(np.array(features).reshape(1,-1))
+        test_img = cv2.resize(img[window[0][1]:window[1][1], window[0][0]:window[1][0]], (64, 64))    
+        features = single_img_features(test_img, color_space, orient, pix_per_cell, cell_per_block, hog_channel)  
+
+        scaler = StandardScaler().fit(np.array(features).reshape(-1,1))
         test_features = scaler.transform(np.array(features).reshape(1, -1))
-        #6) Predict using your classifier
+
         prediction = svc.predict(test_features)
-        print ("Prediction: ", prediction)
-        #7) If positive (prediction == 1) then save the window
+
         if prediction == 1:
             on_windows.append(window)
     
-    #8) Return windows for positive detections
     return on_windows
 
 def add_heat(heatmap, bbox_list):
@@ -265,7 +240,7 @@ def add_heat(heatmap, bbox_list):
 
     # Return updated heatmap
     return heatmap
-    
+
 def apply_threshold(heatmap, threshold):
     # Zero out pixels below the threshold
     heatmap[heatmap <= threshold] = 0
@@ -292,24 +267,20 @@ def draw_labeled_bboxes(img, labels):
     # Return the image
     return img
 
-
-#image = image.astype(np.float32)/255
-
 def detect_car(image_to_process):
     draw_image = np.copy(image_to_process)
     
     heat = np.zeros_like(image_to_process[:,:,0]).astype(np.float)
     
     windows = slide_window(image_to_process,x_start_stop=[0, 1280], y_start_stop=[380, 680], xy_window=(128, 128), xy_overlap=(0.65, 0.65))
-    print ("windows: ", windows)    
+        
     #draw_image = draw_boxes(draw_image, windows, color=(0, 255, 255))
             
     hot_windows = search_windows(image_to_process, windows)
-    print ("Hot Windows: ", hot_windows)
     # Add heat to each box in box list
     add_heat(heat, hot_windows)
     
-    windows = slide_window(image_to_process,x_start_stop=[0, 1280], y_start_stop=[390, 620], xy_window=(96, 96), xy_overlap=(0.65, 0.65))
+    windows = slide_window(image_to_process, x_start_stop=[0, 1280], y_start_stop=[390, 620], xy_window=(96, 96), xy_overlap=(0.65, 0.65))
         
     #draw_image = draw_boxes(draw_image, windows, color=(0, 255, 0))
             
@@ -328,26 +299,24 @@ def detect_car(image_to_process):
     
     windows = slide_window(image_to_process,x_start_stop=[0, 1280], y_start_stop=[390, 500], xy_window=(64, 64), xy_overlap=(0.65, 0.65))
         
-    #draw_image = draw_boxes(draw_image, windows, color=(255, 0, 0))
             
     hot_windows = search_windows(image_to_process, windows)
-    # Add heat to each box in box list
     add_heat(heat, hot_windows)
     
-    # Apply threshold to help remove false positives
     heat = apply_threshold(heat, 2)
 
-    # Visualize the heatmap when displaying    
     heatmap = np.clip(heat, 0, 255)
 
-    # Find final boxes from heatmap using label function
     labels = label(heatmap)
     
     draw_labeled_bboxes(draw_image, labels) 
     
     return draw_image, heatmap, labels
 
-image = mpimg.imread('test_images/test6.jpg')
+
+
+
+image = mpimg.imread('test_images/test1.jpg')
 detected_car_image, detected_car_heatmap, detected_car_labales = detect_car(image)
 
 figure, (detected_car_image_plot, detected_car_heatmap_plot, detected_car_labales_plot) = plt.subplots(1, 3, figsize=(20,15))
